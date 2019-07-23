6858555b64160cb141b0e1053b63dad798510f19
david harlow <david.harlow@thetin.net>
2019-07-23 16:49:54 +0100

    implemented option to target all XML elements

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # XmlFindReplace
 
-Xml attribute value replacer - built specifically for a TeamCity project import with mismatch of versions and varying attrbiute values between the two. Will be built to be more generic in future. 
+Xml attribute value replacer - built specifically for a TeamCity project import with mismatch of versions and the XML schemas, built for a specific use case but hoping to make more generic in future. 
+
+Configuration is currently done in the `appsettings.json`, properties are defined below:
+- `folderPath` The base folder path for the program to scan for your XML files
+- `findText` The text that is being searched for
+- `replaceText` The text that we are going to replace the findText for
+- `elementName` The XML element that we are specifically looking to target, leave blank if you'd like to target all elements
+- `attributeName` The attribute that we are looking to target
+
--- Xml/XmlFileService.cs
+++ Xml/XmlFileService.cs
@@ -38,7 +38,7 @@ namespace FindReplace.Xml
 					UpdateFile(file.FullName);
 				}
 			}
-
+			 
 			var directories = directory.GetDirectories();
 
 			if (directories.Length > 0)
@@ -55,44 +55,63 @@ namespace FindReplace.Xml
 			if (File.Exists(filePath))
 			{
 				var xmlFile = new XmlDocument();
-				var isUpdated = false;
+				var hasFileUpdated = false;
 
 				try
 				{
 					xmlFile.Load(filePath);
 
-					var xmlElements = xmlFile.GetElementsByTagName(ElementName);
+					// If no element name found in config, search through all elements
+					var replaceAllElements = string.IsNullOrEmpty(ElementName);
+					var xmlNodes = replaceAllElements ? xmlFile.ChildNodes : xmlFile.GetElementsByTagName(ElementName);
 
-					foreach (XmlElement xmlElement in xmlElements)
-					{
-						var xmlElementAttributeValue = xmlElement.GetAttribute(AttributeName);
-
-						if (xmlElementAttributeValue != null && xmlElementAttributeValue == FindText)
-						{
-							xmlElement.SetAttribute(AttributeName, ReplaceText);
-							isUpdated = true;
-						}
-					}
+					hasFileUpdated = UpdateXmlNodes(xmlNodes, replaceAllElements, hasFileUpdated);
 				}
 				catch (Exception ex)
 				{
 					Console.WriteLine($"Error attemting to parse XML document: {filePath}, {ex.Message}");
 				}
 
-
 				try
 				{
-					if (isUpdated)
+					if (hasFileUpdated)
 					{
 						xmlFile.Save(filePath);
 						Console.WriteLine($"File {filePath} has been modfied");
 					}
 				}
+
 				catch (Exception ex)
 				{
 					Console.WriteLine($"Error attemting to save XML document: {filePath}, {ex.Message}");
 				}
 			}
+		}
+
+		private static bool UpdateXmlNodes(XmlNodeList nodes, bool searchAll, bool hasFileUpdated)
+		{
+			foreach (var node in nodes)
+			{
+				var xmlElement = node as XmlElement;
+
+				if (xmlElement != null)
+				{
+					var xmlElementAttributeValue = xmlElement.GetAttribute(AttributeName);
+
+					if (xmlElementAttributeValue != null && xmlElementAttributeValue == FindText)
+					{
+						xmlElement.SetAttribute(AttributeName, ReplaceText);
+						hasFileUpdated = true;
+					}
+
+					if (xmlElement.HasChildNodes && searchAll)
+					{
+						UpdateXmlNodes(xmlElement.ChildNodes, true, hasFileUpdated);
+					}
+				}
+			}
+
+			return hasFileUpdated;
 		}
 	}
 }
